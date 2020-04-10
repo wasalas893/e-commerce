441d86d81e44d9680774c5a058c832b7cce95448
wasalas <wasalas893@gmail.com>
2020-04-10 12:42:25 +0530

    Updates

---
+++ precache-manifest.683ffcdfbc52ee59c11f945557b590f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2d3df36f2aba826042c925473b8a6f",
+    "url": "/wasalas893/e-commerce.git/index.html"
+  },
+  {
+    "revision": "e2a5d3e53fed32231d49",
+    "url": "/wasalas893/e-commerce.git/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4f108cbf2332db70f6fc",
+    "url": "/wasalas893/e-commerce.git/static/css/main.526068da.chunk.css"
+  },
+  {
+    "revision": "e2a5d3e53fed32231d49",
+    "url": "/wasalas893/e-commerce.git/static/js/2.04e59db0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wasalas893/e-commerce.git/static/js/2.04e59db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f108cbf2332db70f6fc",
+    "url": "/wasalas893/e-commerce.git/static/js/main.e21d2672.chunk.js"
+  },
+  {
+    "revision": "a9d83ee01da431184f87",
+    "url": "/wasalas893/e-commerce.git/static/js/runtime-main.2ecdbfe5.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/wasalas893/e-commerce.git/static/media/logo.9f7e506d.svg"
+  }
+]);
